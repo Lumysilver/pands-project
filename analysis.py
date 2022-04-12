62f170408a7e55dca23ff454f5f02469b0dca4c4
Luminita Done <lumysilver@gmail.com>
2022-04-12 17:40:27 +0100

    added data

---
+++ analysis.py
@@ -0,0 +1,9 @@
+#I will start the research on the data set online and write a summary
+# Download the data set and add it to my repository.
+#Research what data libraries will help me achieve the results I want; 
+
+import pandas as pd #we installed 
+
+
+
+
