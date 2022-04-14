8d49e9f593c708b6a5c428a0a2b57b2100f4e6bb
Luminita Done <lumysilver@gmail.com>
2022-04-14 01:24:42 +0100

    opened and read the csv file

--- analysis.py
+++ analysis.py
@@ -2,8 +2,25 @@
 # Download the data set and add it to my repository.
 #Research what data libraries will help me achieve the results I want; 
 
-import pandas as pd #we installed 
+#importing libraries that will help with the data set
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+#we are going to load the data we have called iris.data using import cvs
+#that loads tha data right into a panda data frame
+#data is called the data frame we have
 
+import csv
+file = open("iris.data.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
 
